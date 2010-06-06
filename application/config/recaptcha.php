0bcb0fb7c7ef53847b9616beafb9d282ebd13f85
immie <isantoso@gmail.com>
2010-06-07 00:04:27 +1000

    change recaptcha theme to: clean

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -13,6 +13,6 @@ $config['recaptcha'] = array(
   'RECAPTCHA_API_SERVER' =>'http://api.recaptcha.net',
   'RECAPTCHA_API_SECURE_SERVER'=>'https://api-secure.recaptcha.net',
   'RECAPTCHA_VERIFY_SERVER' =>'api-verify.recaptcha.net',
-  'theme' => 'white'
+  'theme' => 'clean'
 );
 
